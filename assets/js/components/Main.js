2cf1ef7f7ba297d9a91cc634dcfd9a81aac93eb1
David Flegal <flegal.david@gmail.com>
2018-08-10 18:45:55 -0700

    moved load notes to main component

--- assets/js/components/Main.js
+++ assets/js/components/Main.js
@@ -4,7 +4,7 @@ import { Button, Form, FormGroup, Label, Input, FormText, Alert, Container, Row,
 
 import Nav from './Nav';
 import NoteModal from './NoteModal';
-import NoteCanvas from './NoteCanvas';
+import Note from './Note';
 
 export default class Main extends Component {
   constructor(props) {
@@ -12,17 +12,44 @@ export default class Main extends Component {
 
     this.state = {
       viewSelected: null,
+      selectedNote: null,
       user: null,
-      noteModalOpen: false
+      noteModalOpen: false,
+      notes: [],
+      returnedId: 0
     }
 
     this.selectView = this.selectView.bind(this);
+    this.loadNotes = this.loadNotes.bind(this);
   }
 
   selectView(event) {
     this.setState({
       viewSelected: event.target.id
     }, function(){console.log(this.state)})
+  }
+
+  loadNotes() {
+    var api = new(API);
+    var token = localStorage.getItem('ntkr.tkn');
+    api.getUserNotes(token)
+      .then(data => {
+        var notes = [];
+        data.map( note => {
+          notes.push(note);
+        })
+        this.setState({
+          notes: notes
+        }, function(){console.log(this.state)})
+      })
+      .catch(data => {
+        console.log("Error");
+        console.log(data);
+      })  
+  }
+
+  componentDidMount() {
+    this.loadNotes();
   }
 
   render() {
@@ -42,7 +69,8 @@ export default class Main extends Component {
             color=" "
             reminder=" "
             font=" "
-            selectView={this.selectView} />
+            selectView={this.selectView}
+            loadNotes={this.loadNotes} />
 
           }
           <Container>
@@ -54,7 +82,31 @@ export default class Main extends Component {
               </Col>
 
               <Col xs="9" className="main-viewer">
-                < NoteCanvas />
+                <div>
+                <Container>
+                  <Row>
+                  {this.state.notes.map( (note, index) => {
+                    var noteNormalized = note[index.toString()];
+                    var pk = noteNormalized["properties"]["note"];
+                    var divId = "note-" + pk;
+                    return (
+                    <Col sm={{ size: 'auto', offset: 1 }}>
+                          <div className="editNote" id={divId}>
+                            < Note
+                              pk={noteNormalized["contents"]["pk"]}
+                              title={noteNormalized["properties"]["title"]}
+                              contents={noteNormalized["contents"]["contents"]}
+                              color=" "
+                              reminder=" "
+                              font=" "
+                               />
+                          </div>
+                    </Col>
+                    )
+                  })}
+                  </Row>
+                </Container>
+                </div>
               </Col>
 
             </Row>
